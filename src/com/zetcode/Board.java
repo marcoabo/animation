704dd2118d34d7aa81798a68a7a77402a4b377c9
marcoabo <marcoabo@yahoo.com>
2018-09-03 23:29:46 -0300

    Version 2 - Utility Timer

--- src/com/zetcode/Board.java
+++ src/com/zetcode/Board.java
@@ -5,68 +5,73 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
-public class Board extends JPanel implements ActionListener {
-	
+public class Board extends JPanel {
+	private static final long serialVersionUID = 1L;
+
 	private final int B_WIDTH = 350;
 	private final int B_HEIGHT = 350;
 	private final int INITIAL_X = -40;
 	private final int INITIAL_Y = -40;
-	private final int DELAY = 25;
+	private final int INITIAL_DELAY = 100;
+	private final int PERIOD_INTERVAL = 25;
+
 	private Image star;
 	private Timer timer;
 	private int x, y;
-	
+
 	public Board() {
 		initBoard();
 	}
-	
+
 	private void loadImage() {
 		ImageIcon ii = new ImageIcon("src/resources/star.png");
 		star = ii.getImage();
 	}
-	
+
 	private void initBoard() {
 		setBackground(Color.BLACK);
 		setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
 		setDoubleBuffered(true);
-		
+
 		loadImage();
-		
+
 		x = INITIAL_X;
 		y = INITIAL_Y;
-		
-		timer = new Timer(DELAY, this);
-		timer.start();
+
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new ScheduleTask(), INITIAL_DELAY, PERIOD_INTERVAL);
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
 		drawStar(g);
 	}
-	
+
 	public void drawStar(Graphics g) {
 		g.drawImage(star, x, y, this);
 		Toolkit.getDefaultToolkit().sync();
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		x += 1;
-		y += 1;
-		
-		if (y > B_HEIGHT) {
-			x = INITIAL_X;
-			y = INITIAL_Y;
+	private class ScheduleTask extends TimerTask {
+
+		@Override
+		public void run() {
+			x += 1;
+			y += 1;
+
+			if (y > B_HEIGHT) {
+				x = INITIAL_X;
+				y = INITIAL_Y;
+			}
+
+			repaint();
 		}
-		
-		repaint();
 	}
 }
